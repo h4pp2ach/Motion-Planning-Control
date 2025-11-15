f898739c1d02a348a740ba7fb1d788ff9374e51a
h4pp2ach <jin4god@inu.ac.kr>
2025-11-15 11:08:02 +0900

    Path Planning_Dijkstra 구현

--- 04_PathPlanning/ex04_Dijkstra.py
+++ 04_PathPlanning/ex04_Dijkstra.py
@@ -2,7 +2,8 @@ import numpy as np
 import math
 import matplotlib.pyplot as plt
 import random
-from map_1 import map
+import heapq
+from map_2 import map
 
 show_animation  = True
 
@@ -15,49 +16,88 @@ class Node:
     def __eq__(self, other):
         if self.position == other.position:
             return True
+    
+    def __lt__(self, other):
+        return self.f < other.f
                 
 def get_action():
-    # action = [dx, dy, cost]
-    # action_set = [action1, action2, ...]
+    
+    diagCost = np.sqrt(2)
+    action_set = [[ 0, 1, 1],
+                  [ 0,-1, 1],
+                  [ 1, 0, 1],
+                  [-1, 0, 1],
+                  [ 1, 1, diagCost],
+                  [ 1,-1, diagCost],
+                  [-1, 1, diagCost],
+                  [-1,-1, diagCost]]
+
     return action_set
 
-def collision_check(omap, node):
-    # Check if node position == obstacle position
-    return col # True or False
+def collision_check(obstacle_set, pos):
+    return pos in obstacle_set
 
 def dijkstra(start, goal, map_obstacle):
+    obstacle_set = set(zip(map_obstacle[0], map_obstacle[1]))
     
     start_node = Node(None, start)
     goal_node = Node(None, goal)
     
     open_list = []
-    closed_list = []
+    closed_list = set()
     
-    open_list.append(start_node)
-    while open_list is not None:
+    heapq.heappush(open_list, (start_node.f, start_node))
+    
+    while open_list:
+
         # Find node with lowest cost
-                
+        cost, cur_node = heapq.heappop(open_list)
+        
         # If goal, return optimal path
+        if cur_node == goal_node:
+            path = []
+            while cur_node is not None:
+                path.append(cur_node.position)
+                cur_node = cur_node.parent
+            return path[::-1]
         
         # If not goal, move from open list to closed list
+        closed_list.add(cur_node.position)
 
-        # Generate child candidate
         action_set = get_action()
-        for action in action_set:
+        for (dx, dy, cost) in action_set:
+
+            new_node = Node(cur_node, (cur_node.position[0] + dx, cur_node.position[1] + dy))
+            
             # If collision expected, do nothing
-
-            # If not collision, create child node
-
+            isCollision = collision_check(obstacle_set, new_node.position)
+            if isCollision:
+                continue
+            
             # If already in closed list, do nothing
-
+            if new_node.position in closed_list:
+                continue
+            
+            new_node.f = cur_node.f + cost
+            
+            # If a better path already exists, skip
+            skip_flag = False
+            for _, open_node in open_list:
+                if new_node == open_node and new_node.f >= open_node.f:
+                    skip_flag = True
+                    break
+            
+            if skip_flag:
+                continue
+            
             # If not in closed list, update open list
-
+            heapq.heappush(open_list, (new_node.f, new_node))
+            
         # show graph
         if show_animation:
             plt.plot(cur_node.position[0], cur_node.position[1], 'yo', alpha=0.5)
             if len(closed_list) % 100 == 0:
                 plt.pause(0.1)
-                
                 
 
 def main():
